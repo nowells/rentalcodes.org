134143267f8f96e273c82c1cf173dd906e9f801c
Nowell Strite <nowell@strite.org>
2018-08-11 15:53:20 -0400

    Added migration stuff.

---
+++ migrate-to-markdown.js
@@ -0,0 +1,31 @@
+const globby = require('globby');
+const TurndownService = require('turndown');
+const path = require('path');
+const {JSDOM} = require('jsdom');
+const fs = require('fs-extra');
+const turndownService = new TurndownService();
+
+
+(async() => {
+    const paths = await globby(['**/*.html', '!node_modules', '!rentalcodes']);
+    await Promise.all(paths.map(async(htmlPath) => {
+        const html = await fs.readFile(htmlPath, 'utf8');
+        const dom = new JSDOM(html);
+
+        if (dom.window.document.querySelector('#breadcrumb')) {
+            dom.window.document.querySelector('#breadcrumb').remove();
+        }
+        const content = dom.window.document.querySelector("#content").innerHTML;
+
+        if (!content) {
+            console.log(`Skipping ${htmlPath}`);
+            return;
+        }
+
+        const directory = path.join('.markdown-migration', path.dirname(htmlPath));
+        const filename = path.basename(htmlPath).replace(/\.html$/, '.md');
+        await fs.mkdirp(directory);
+        await fs.writeFile(path.join(directory, filename), turndownService.turndown(content));
+    }));
+})();
+
